1ca9605e3ed16064c3bf891645388255c572bfc3
timepp <tongjunhui@live.cn>
2012-08-12 12:04:38 +0800

    增加wtl

--- mill/stdafx.h
+++ mill/stdafx.h
@@ -10,6 +10,7 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
 #include <windows.h>
+#include <shellapi.h>
 
 // C RunTime Header Files
 #include <stdlib.h>
@@ -17,5 +18,12 @@
 #include <memory.h>
 #include <tchar.h>
 
-
-// TODO: reference additional headers your program requires here
+#include <atlbase.h>
+#include <atlcom.h>
+#include <atlwin.h>
+#include <wtl/include/atlapp.h>
+#include <wtl/include/atlres.h>
+#include <wtl/include/atlctrls.h>
+#include <wtl/include/atlctrlx.h>
+#include <tplib/include/service.h>
+#include <tplib/include/pinyin.h>
